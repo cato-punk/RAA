bb7fc2dfd3cb3bc64c83fb567b87c01c287464f5
Catalina <c.quidel04@ufromail.cl>
2025-06-21 17:20:17 -0400

    3

--- src/main/java/Modelo/Persona.java
+++ src/main/java/Modelo/Persona.java
@@ -1,4 +1,79 @@
 package Modelo; //clase abstracta es clase padre o Super
 
-public class Persona {
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Persona {
+    private String id;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    //private String sexo; (no veo que esto sea una carcteristica importante)
+    private String direccion;
+    private String numeroTelefono;
+    private String correoElectronico;
+
+    public Persona(String nombre, LocalDate fechaNacimiento, String direccion, String numeroTelefono, String correoElectronico) {
+        this.id = UUID.randomUUID().toString(); // genera iD unico
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        //this.sexo = sexo;
+        this.direccion = direccion;
+        this.numeroTelefono = numeroTelefono;
+        this.correoElectronico = correoElectronico;
+    }
+
+    // nos faltan los constructores de json
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    //public String getSexo() {
+    //    return sexo;
+    // }
+
+    //public void setSexo(String sexo) {
+    //    this.sexo = sexo;
+    //}
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
 }
