071b2b741adca4e72089bbd61105881b92e1c117
Catalina <c.quidel04@ufromail.cl>
2025-06-21 17:38:11 -0400

    3

--- src/main/java/Modelo/Rescatista.java
+++ src/main/java/Modelo/Rescatista.java
@@ -1,4 +1,21 @@
 package Modelo;
 
-public class Rescatista {
+import java.time.LocalDate;
+
+
+public class Rescatista extends Persona { //la dependencia
+    public Rescatista(String nombre, LocalDate fechaNacimiento, String direccion, String numeroTelefono, String correoElectronico) {
+        super(nombre, fechaNacimiento, direccion, numeroTelefono, correoElectronico);
+    }
+
+    // constructor para cargar desde JSON
+    public Rescatista(String id, String nombre, LocalDate fechaNacimiento, String sexo, String direccion, String numeroTelefono, String correoElectronico) {
+        super(id, nombre, fechaNacimiento, sexo, direccion, numeroTelefono, correoElectronico);
+    }
+
+    @Override
+    public String toString() {
+        return "Rescatista - ID: " + getId() + ", Nombre: " + getNombre() + ", Correo: " + getCorreoElectronico();
+    }
+
 }
--- src/main/java/Modelo/Veterinario.java
+++ src/main/java/Modelo/Veterinario.java
@@ -1,4 +1,32 @@
 package Modelo;
+import java.time.LocalDate;
 
-public class Veterinario {
+
+public class Veterinario extends Persona {
+    private String numeroLicencia; // atributo de Veterinario
+
+    public Veterinario(String nombre, LocalDate fechaNacimiento, String sexo, String direccion, String numeroTelefono, String correoElectronico, String numeroLicencia) {
+        super(nombre, fechaNacimiento, sexo, direccion, numeroTelefono, correoElectronico);
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    // constructor para cargar desde JSON
+    public Veterinario(String id, String nombre, LocalDate fechaNacimiento, String sexo, String direccion, String numeroTelefono, String correoElectronico, String numeroLicencia) {
+        super(id, nombre, fechaNacimiento, sexo, direccion, numeroTelefono, correoElectronico);
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    public String getNumeroLicencia() {
+        return numeroLicencia;
+    }
+
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Veterinario - ID: " + getId() + ", Nombre: " + getNombre() + ", Licencia: " + numeroLicencia;
+    }
+
 }
