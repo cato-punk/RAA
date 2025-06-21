5fe9624beb6657d1c37e3edd44f3918655ed85dc
Catalina <c.quidel04@ufromail.cl>
2025-06-21 17:40:41 -0400

    4

--- src/main/java/Modelo/Adoptante.java
+++ src/main/java/Modelo/Adoptante.java
@@ -1,4 +1,20 @@
 package Modelo;
 
-public class Adoptante {
+import java.time.LocalDate;
+
+public class Adoptante extends Persona {
+    public Adoptante(String nombre, LocalDate fechaNacimiento, String sexo, String direccion, String numeroTelefono, String correoElectronico) {
+        super(nombre, fechaNacimiento, sexo, direccion, numeroTelefono, correoElectronico);
+    }
+
+    // constructor para cargar desde JSON
+    public Adoptante(String id, String nombre, LocalDate fechaNacimiento, String sexo, String direccion, String numeroTelefono, String correoElectronico) {
+        super(id, nombre, fechaNacimiento, sexo, direccion, numeroTelefono, correoElectronico);
+    }
+
+    @Override
+    public String toString() {
+        return "Adoptante - ID: " + getId() + ", Nombre: " + getNombre() + ", Correo: " + getCorreoElectronico();
+    }
+
 }
