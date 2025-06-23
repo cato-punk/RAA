b497f0a9bee70fb3af98046672c210dd4718b18c
natalia <n.perez15@ufromail.cl>
2025-06-22 22:17:18 -0400

    Commit

--- src/main/java/Modelo/Veterinario.java
+++ src/main/java/Modelo/Veterinario.java
@@ -1,4 +1,47 @@
 package Modelo;
 
-public class Veterinario {
+import java.time.LocalDate;
+
+public class Veterinario extends Persona {
+    private String especialidad;
+    private String licencia; // Nuevo atributo
+
+    // Constructor completo, pasando todos los atributos de Persona y los propios de Veterinario
+    public Veterinario(String nombre, String rut, LocalDate fechaNacimiento, String direccion,
+                       String numeroTelefono, String correoElectronico, String especialidad, String licencia) {
+        super(nombre, rut, fechaNacimiento, direccion, numeroTelefono, correoElectronico);
+        this.especialidad = especialidad;
+        this.licencia = licencia;
+    }
+
+
+    public Veterinario(String nombre, String rut, String especialidad, String licencia) {
+        super(nombre, rut); // Llama al constructor de Persona con nombre y rut
+        this.especialidad = especialidad;
+        this.licencia = licencia;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Veterinario [" + super.toString() +
+                ", Especialidad: " + especialidad +
+                ", Licencia: " + licencia + "]";
+    }
 }
+
